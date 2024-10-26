f8a25697fbbe0dfa0aec01feb07ce7c303917fe8
maxsve18 <svensson.max@gmail.com>
2024-10-26 13:12:02 +0200

    added call start method to numberTOGuess and package

--- GuessingGame.java
+++ GuessingGame.java
@@ -1,9 +1,9 @@
 
+package guessingGame;
 public class GuessingGame {
     public static void main(String[] args) {
         Guesser numberToGuess = new Guesser(0,1000);
-
-
+        numberToGuess.start();
 
     }
 }
