d7ebcf72d7532a419cd5c1f5aa05f64f50ad10d0
maxsve18 <svensson.max@gmail.com>
2024-10-28 14:03:20 +0100

    removed package

--- GuessingGame.java
+++ GuessingGame.java
@@ -1,6 +1,6 @@
+
 import java.io.Console;
 import java.util.*;
-package guessingGame;
 public class GuessingGame {
     public static void main(String[] args) {
         Guesser numberToGuess = new Guesser(0,10);
